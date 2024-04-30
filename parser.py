0dabd66631981851ef0e5f1bb7d1f6515a19a39b
叶璨铭 <12011404@mail.sustech.edu.cn>
2024-05-01 03:27:11 +0800

    feat: auto batchsize for evaluation

--- parser.py
+++ parser.py
@@ -1,25 +1,45 @@
 
 import argparse
-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 def parse_arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("--positive_dist_threshold", type=int, default=25,
                         help="distance (in meters) for a prediction to be considered a positive")
-    parser.add_argument("--method", type=str, default="cosplace",
+    parser.add_argument("--method", type=str, 
+                        # default="cosplace",
+                        default="eigenplaces",
                         choices=["netvlad", "sfrs", "cosplace", "convap", "mixvpr", "eigenplaces",
-                                 "anyloc", "salad"],
+                                 "anyloc", "salad", 
+                                 "deep_vpr_model"],
                         help="_")
-    parser.add_argument("--backbone", type=str, default=None,
+    parser.add_argument("--backbone", type=str, 
+                        # default=None,
+                        default="ResNet18",
                         choices=[None, "VGG16", "ResNet18", "ResNet50", "ResNet101", "ResNet152"],
                         help="_")
-    parser.add_argument("--descriptors_dimension", type=int, default=None,
+    parser.add_argument("--descriptors_dimension", type=int, 
+                        # default=None,
+                        default=512,
                         help="_")
-    parser.add_argument("--database_folder", type=str, required=True,
+    parser.add_argument("--database_folder", type=str, 
+                        # required=True,
+                        default="../VPR-datasets-downloader/datasets/pitts30k/database",
                         help="path/to/database")
-    parser.add_argument("--queries_folder", type=str, required=True,
-                        help="path/to/queries")
+    parser.add_argument("--queries_folder", type=str, 
+                        # required=True,
+                        default="../VPR-datasets-downloader/datasets/pitts30k/queries",
+                        help="path/to/queries"
+                        )
+    parser.add_argument("--resume_eval", type=str,
+                        # default=None,
+                        default="logs/default/2024-04-25_02-20-26",
+                        help="for loading. "
+                        )
     parser.add_argument("--num_workers", type=int, default=4,
                         help="_")
     parser.add_argument("--batch_size", type=int, default=4,
@@ -37,6 +57,23 @@ def parse_arguments():
                         "wrongly predicted queries")
     
     args = parser.parse_args()
+    
+    # Automatically determine the batch size based on the GPU memory
+    method_memory_efficiency = 1 # 如果方法特别占用显存, 可以设置为更大的值
+    num_gpus = torch.cuda.device_count()
+    average_memory = 0
+    for i in range(num_gpus):
+        device = torch.device(f"cuda:{i}")
+        properties = torch.cuda.get_device_properties(device)
+        print(f"GPU {i} 的详细信息：")
+        print("名称：", properties.name)
+        print("显存大小：", properties.total_memory)
+        lamb = 1/(i+1)
+        average_memory = (1-lamb)*average_memory+lamb*properties.total_memory
+    # args.train_batch_size = 20
+    batch_size = args.batch_size/1*num_gpus*average_memory/1024/1024/1024/80/method_memory_efficiency
+    args.batch_size = int(batch_size)
+    print(f"根据显存, 自动智能决定batch_size为: {args.batch_size}")
     
     if args.method == "netvlad":
         if args.backbone not in [None, "VGG16"]:
@@ -98,7 +135,7 @@ def parse_arguments():
         if args.backbone in ["ResNet50", "ResNet101", "ResNet152"] and args.descriptors_dimension not in [128, 256, 512, 2048]:
             raise ValueError(f"When using EigenPlaces with {args.backbone} the descriptors_dimension must be in [128, 256, 512, 2048]")
     
-    elif args.method == "anyloc":
+    elif args.method == "anyloc" or args.method == "deep_vpr_model":
         args.backbone = "DINOv2"
         args.descriptors_dimension = 49152
     
