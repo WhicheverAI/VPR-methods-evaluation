0349aac3d06025247fbc5bd77fd0735b45036a6a
叶璨铭 <12011404@mail.sustech.edu.cn>
2024-05-01 03:32:04 +0800

    feat: supports for evaluating deep-benchmark

--- vpr_models/__init__.py
+++ vpr_models/__init__.py
@@ -4,7 +4,7 @@ import torch
 from vpr_models import sfrs, salad, anyloc, convap, mixvpr, netvlad
 
 
-def get_model(method, backbone=None, descriptors_dimension=None):
+def get_model(method, backbone=None, descriptors_dimension=None, args=None):
     if method == "sfrs":
         model = sfrs.SFRSModel()
     elif method == "netvlad":
@@ -23,6 +23,16 @@ def get_model(method, backbone=None, descriptors_dimension=None):
         model = anyloc.AnyLocWrapper()
     elif method == "salad":
         model = salad.SaladWrapper()
-    
+    elif method == "deep_vpr_model":
+        model, deep_vpr_model_config = torch.hub.load('../deep-visual-geo-localization-benchmark', 
+                       'deep_vpr_model', 
+                       source='local',
+                    #    source='github',
+                       )
+        model = anyloc.AnyLocWrapper(model=model, resize=[224, 224])
+        import json
+        deep_vpr_model_config_str = json.dumps(deep_vpr_model_config, indent=4)
+        with open(f"{args.output_folder}/deep_vpr_model_config.json","w") as f:
+            f.write(deep_vpr_model_config_str)
     return model
 
--- vpr_models/anyloc.py
+++ vpr_models/anyloc.py
@@ -4,16 +4,24 @@ import torch
 import torchvision.transforms as transforms
 
 
+first_time_run = True
 class AnyLocWrapper(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, model=None, resize=None):
         super().__init__()
-        self.model = torch.hub.load("AnyLoc/DINO", "get_vlad_model", backbone="DINOv2", device="cuda")
+        self.model = model or torch.hub.load("AnyLoc/DINO", "get_vlad_model", backbone="DINOv2", device="cuda")
+        self.resize = resize
     def forward(self, images):
-        b, c, h, w = images.shape
-        # DINO wants height and width as multiple of 14, therefore resize them
-        # to the nearest multiple of 14
-        h = round(h / 14) * 14
-        w = round(w / 14) * 14
-        images = transforms.functional.resize(images, [h, w], antialias=True)
+        if self.resize:
+            images = transforms.functional.resize(images, self.resize, antialias=True)
+        else:
+            b, c, h, w = images.shape
+            if first_time_run: 
+                first_time_run = False
+                print(f"AnyLocWrapper: input shape: {images.shape}")
+            # DINO wants height and width as multiple of 14, therefore resize them
+            # to the nearest multiple of 14
+            h = round(h / 14) * 14
+            w = round(w / 14) * 14
+            images = transforms.functional.resize(images, [h, w], antialias=True)
         return self.model(images)
 
